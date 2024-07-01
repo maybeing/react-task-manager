31d7260ba1bbccbd419d7d7ef5ae46ec0bf0deef
Michelle Pani <michelle.pani@shiftsistemas.com.br>
2024-07-01 15:39:58 -0300

    Estrutura básica do app

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={{ flex: 1}}>
+
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+
+        <Text style={{ fontSize: 24, textAlign: 'center' }}>
+          Feed
+        </Text>
+        <View style={{ height: 500, backgroundColor: '#e5e5e5', width: '100%', marginTop: 20 }}>
+          <ScrollView contentContainerStyle={{ alignItems: 'center' }}>
+          <View style={{ width: 300, height: 300, backgroundColor: 'red' }} />
+          <View style={{ width: 300, height: 300, backgroundColor: 'green' }} />
+          <View style={{ width: 300, height: 300, backgroundColor: 'blue' }} />
+          <View style={{ width: 300, height: 300, backgroundColor: 'red' }} />
+          <View style={{ width: 300, height: 300, backgroundColor: 'green' }} />
+          <View style={{ width: 300, height: 300, backgroundColor: 'blue' }} />
+          </ScrollView>
+        </View>
+        <StatusBar style={{ color:'green'}} />
+      </View>
+
+      <View style={{alignItems: 'flex-end', justifyContent:'space-around', flexDirection: 'row', gap: 40, marginBottom: 30}}>
+        <Text> Explorar </Text>
+        <Text> Tarefas </Text>
+        <Text> Perfil </Text>
+      </View>
+
+  </View>
   );
 }
+
+/* return (
+  <View style={styles.container}>
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+  <Text style={styles.text}>Hello World</Text>
+  <StatusBar style="auto" />
+
+    <View style={{ width: 100, height: 100, backgroundColor: 'orange'}}></View>
+    <View style={{ width: 100, height: 100, backgroundColor: 'purple'}}></View>
+  </View>
+</View>
+);
+*/
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#a992d4',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 30,
   },
 });
